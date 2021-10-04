c63a6b5ac191c815f9f941da32557c23de423773
silascode132 <silascode132@gmail.com>
2021-10-04 10:08:02 +0700

    categories, review, blogs and footer. finish home

--- js/script.js
+++ js/script.js
@@ -60,4 +60,26 @@ var swiper = new Swiper(".product-slider", {
         slidesPerView: 3,
       },
     },
-  });
+});
+
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
+
